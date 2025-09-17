02fd55fe280da54946f34205abb58fbd275b9a5d
Antonio Lucas <antoniolucas9014@gmail.com>
2025-09-16 22:24:24 -0300

    fix: corrigindo layout e removendo contextos desnecessários

--- src/App-Minimal.tsx
+++ src/App-Minimal.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "@/components/layout/Header";
 import Index from "./pages/Index";
 import Produtos from "./pages/Produtos";
 import Entradas from "./pages/Entradas";
@@ -15,15 +16,26 @@ const App = () => (
       {/* Rota de Login */}
       <Route path="/login" element={<Login />} />
       
-      {/* Rotas principais */}
-      <Route path="/" element={<Index />} />
-      <Route path="/produtos" element={<Produtos />} />
-      <Route path="/entradas" element={<Entradas />} />
-      <Route path="/saidas" element={<Saidas />} />
-      <Route path="/movimentacoes" element={<Movimentacoes />} />
-      <Route path="/relatorios" element={<Relatorios />} />
-      <Route path="/configuracoes" element={<Configuracoes />} />
-      <Route path="*" element={<NotFound />} />
+      {/* Rotas principais com layout */}
+      <Route path="/*" element={
+        <div className="min-h-screen bg-background">
+          <Header />
+          <div className="pt-20">
+            <div className="flex-1">
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/produtos" element={<Produtos />} />
+                <Route path="/entradas" element={<Entradas />} />
+                <Route path="/saidas" element={<Saidas />} />
+                <Route path="/movimentacoes" element={<Movimentacoes />} />
+                <Route path="/relatorios" element={<Relatorios />} />
+                <Route path="/configuracoes" element={<Configuracoes />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      } />
     </Routes>
   </BrowserRouter>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App-Minimal.tsx'
 import './fonts.css'
 import './index.css'
-import { DataProvider } from './contexts/DataContext'
-import { AuthProvider } from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <DataProvider>
-        <App />
-      </DataProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>,
 )
