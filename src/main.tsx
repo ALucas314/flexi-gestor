a1f83f4892a3055313cc395bea84cd84790fc1ef
Antonio Lucas <antoniolucas9014@gmail.com>
2025-09-16 22:26:18 -0300

    restore: voltando para versão original completa com Firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App-Minimal.tsx'
+import App from './App.tsx'
 import './fonts.css'
 import './index.css'
+import { DataProvider } from './contexts/DataContext'
+import { AuthProvider } from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
